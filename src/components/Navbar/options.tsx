8f83047013991e7d8e95c8b2c0d4520368770647
Carlos Silva <carlosemilio.contato@gmail.com>
2025-01-02 22:50:51 -0300

    create navbar

---
+++ src/components/Navbar/options.tsx
@@ -0,0 +1,9 @@
+export function Options() {
+  return (
+      <ul className="flex flex-row space-x-8 text-content-heading ">
+        <li className="font-bold"> Início </li>
+        <li className="text-content-headline"> Preço </li>
+        <li className="text-content-headline"> FAQ </li>
+      </ul>
+  )
+}
---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import { Button } from './Button'
+import { Input } from './Input'
+import { Textarea } from './Textarea'
+import { Hero } from './Hero'
+
+export {
+  Button,
+  Input,
+  Textarea,
+  Hero
+}
