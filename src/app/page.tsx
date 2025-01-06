bc223f67674cae40985f96c6ccc0e86ca53f5d7a
Carlos Silva <carlosemilio.contato@gmail.com>
2025-01-05 23:30:24 -0300

    fix: layout size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Hero } from "@/components";
 import { LandingPage } from "@/components/LandingPage";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="max-w-6xl mx-auto px-4 py-8 flex space-y-8 flex-col">
+    <div className="max-w-7xl mx-auto px-4 py-8 flex space-y-8 flex-col">
       <Navbar/>
       <LandingPage/>      
     </div>
--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,13 +1,9 @@
-import { FAQ } from "./FAQ"
-import { Header } from "./Header"
 import { Hero } from "./Hero"
 
 export function LandingPage() {
   return (
     <div className="flex flex-col space-y-8">
       <Hero/>
-      {/* <Header /> */}
-      {/* <FAQ /> */}
     </div>
   )
 }
