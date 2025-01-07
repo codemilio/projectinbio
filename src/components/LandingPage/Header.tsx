cdfece3c454784c970859d1a472495c17b36eb08
Carlos Silva <carlosemilio.contato@gmail.com>
2025-01-06 22:19:38 -0300

    fix: header component

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -2,7 +2,7 @@
 
 export function Header() {
   return (
-    <header className="flex flex-col space-y-8 items-center text-center mt-20">
+    <header className="flex flex-col space-y-8 items-center text-center mt-40">
       <span className="py-2 px-8 bg-background-secondary w-fit rounded-full text-content-heading"> 7 dias grátis </span>
       <h1 className="font-bold text-7xl"> Crie seu portfólio e linke seus projetos em minutos! </h1>
       <h3 className="text-xl max-w-2xl"> Mostre seu trabalho de forma profissional e simplifique sua presença online com uma plataforma fácil e personalizável. </h3>
